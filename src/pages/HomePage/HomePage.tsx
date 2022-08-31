265c4bf69362d194c4dabb482f6e1e9937f3af66
igorslepenkov <slepenkov.nii@yandex.by>
2022-08-31 15:37:12 +0300

    feat(docker): add dockerfile, .dockerignore and docker-compose.yml

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Page } from "../../components/Page";
 import { Title } from "../../components/Title";
+import { ROUTE } from "../../router";
 import { Color } from "../../ui";
 import { GreetingWrapper } from "./style";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    console.log("Hello");
+    navigate(ROUTE.REGISTER);
+  };
   return (
     <Page>
       <GreetingWrapper>
@@ -14,7 +21,9 @@ export const HomePage = () => {
         <Title grade={1} color={Color.DarkBluePrim}>
           We really are care about you health!
         </Title>
-        <Button type="button">Let's find your top doctor</Button>
+        <Button type="button" onClick={handleButtonClick}>
+          Register or sign in to continue
+        </Button>
       </GreetingWrapper>
     </Page>
   );
